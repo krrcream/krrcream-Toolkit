b67d1bb944e7358f5badc2a72ac1134c6983c204
LA <1245661240@qq.com>
2025-10-04 19:43:09 +0800

    规范格式

--- UI/ProcessingWindow.xaml.cs
+++ UI/ProcessingWindow.xaml.cs
@@ -1,19 +1,18 @@
 ﻿using System.Windows;
 
-namespace krrTools.UI
-{
-    public partial class ProcessingWindow : Window
-    {
-        public ProcessingWindow()
-        {
-            InitializeComponent();
-        }
+namespace krrTools.UI;
 
-        public void UpdateProgress(int current, int total)
-        {
-            progressBar.Maximum = total;
-            progressBar.Value = current;
-            progressText.Text = $"进度: {current} / {total}";
-        }
+public partial class ProcessingWindow : Window
+{
+    public ProcessingWindow()
+    {
+        InitializeComponent();
+    }
+
+    public void UpdateProgress(int current, int total)
+    {
+        progressBar.Maximum = total;
+        progressBar.Value = current;
+        progressText.Text = $"进度: {current} / {total}";
     }
 }
